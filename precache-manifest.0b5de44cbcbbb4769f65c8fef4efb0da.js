3f903aa6c68e3270ec1000ac886790061f553610
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-09-01 12:00:06 +0200

    Updates

---
+++ precache-manifest.0b5de44cbcbbb4769f65c8fef4efb0da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81bdfb9ed0d6ae416013a3cd73196e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da0326145f9fbf777ba6",
+    "url": "/static/js/2.f12d4858.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f12d4858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fb0e738246563445ee",
+    "url": "/static/js/main.212e8566.chunk.js"
+  },
+  {
+    "revision": "b3ae54b78d50542446b9",
+    "url": "/static/js/runtime-main.e794fc53.js"
+  }
+]);
